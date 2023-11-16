52a2f3f9ba0a35417e66300c2f4dec0dd91fb150
Karken Turarov <karken_turarov@mail.ru>
2023-11-16 23:06:05 +0600

    add Actions add ActionTypes

---
+++ src/Actions.ts
@@ -0,0 +1,27 @@
+import { GET_MESSAGES, RECEIVE_MESSAGES, SEND_MESSAGE, MESSAGE_SENT } from './ActionTypes';
+
+
+interface Message {
+  nickname: string;
+  message: string;
+  date?: Date;
+}
+
+export const getMessages = () => ({
+  type: GET_MESSAGES,
+});
+
+export const receiveMessages = (messages: Message[]) => ({
+  type: RECEIVE_MESSAGES,
+  payload: messages,
+});
+
+export const sendMessage = (message: Message) => ({
+  type: SEND_MESSAGE,
+  payload: message,
+});
+
+export const messageSent = (message: Message) => ({
+  type: MESSAGE_SENT,
+  payload: message,
+});
