94e09007d19e0f06536b159dc42be9cb4ab4f15b
Karken Turarov <karken_turarov@mail.ru>
2023-12-08 11:34:18 +0600

    add case delete tests

--- src/__tests__/middleware.test.ts
+++ src/__tests__/middleware.test.ts
@@ -1,6 +1,5 @@
 import chatMiddleware from '../middleware';
 import { Store } from '../ChatStore';
-import { Message } from '../Actions';
 import { jest } from '@jest/globals';
 import * as chatApi from '../chat';
 import {
@@ -11,30 +10,52 @@ import {
 jest.mock('../chat');
 
 describe('chatMiddleware', () => {
-  let storeMock: Store;
-  let nextMock: jest.Mock;
-  let actionMock: ChatActionTypes;
+    let storeMock: Store;
+    let nextMock: jest.Mock;
+    let actionMock: ChatActionTypes;
 
-  beforeEach(() => {
-    storeMock = { dispatch: jest.fn() } as unknown as Store;
-    nextMock = jest.fn();
-    const sendMessageMock = jest.mocked(chatApi.sendMessage);
-    sendMessageMock.mockResolvedValue({
-        name: 'uniqueMessageId'
-      });
-  });
+    beforeEach(() => {
+        storeMock = { dispatch: jest.fn() } as unknown as Store;
+        nextMock = jest.fn();
+        const sendMessageMock = jest.mocked(chatApi.sendMessage);
+        sendMessageMock.mockResolvedValue({
+            name: 'uniqueMessageId'
+        });
+    });
 
-  it('should handle SEND_MESSAGE action', async () => {
-    const testMessage = { id: '1', nickname: 'User', message: 'Test', date: new Date() };
-    actionMock = { type: 'SEND_MESSAGE', payload: testMessage } as SendMessageAction;
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
 
-    const middleware = chatMiddleware(storeMock);
-    await middleware(nextMock)(actionMock);
+    it('should handle SEND_MESSAGE action', async () => {
+        const testMessage = { id: '1', nickname: 'User', message: 'Test', date: new Date() };
+        actionMock = { type: 'SEND_MESSAGE', payload: testMessage } as SendMessageAction;
 
-    expect(chatApi.sendMessage).toHaveBeenCalledWith(testMessage);
-    expect(storeMock.dispatch).toHaveBeenCalledWith({ type: 'MESSAGE_SENT', payload: testMessage });
-    expect(storeMock.dispatch).toHaveBeenCalledWith({ type: 'GET_MESSAGES' });
-  });
+        const middleware = chatMiddleware(storeMock);
+        await middleware(nextMock)(actionMock);
 
-  
+        expect(chatApi.sendMessage).toHaveBeenCalledWith(testMessage);
+        expect(storeMock.dispatch).toHaveBeenCalledWith({ type: 'MESSAGE_SENT', payload: testMessage });
+        expect(storeMock.dispatch).toHaveBeenCalledWith({ type: 'GET_MESSAGES' });
+    });
+
+    it('should handle GET_MESSAGES action', async () => {
+        const getMessagesListMock = jest.mocked(chatApi.getMessagesList);
+        getMessagesListMock.mockResolvedValue([
+            { id: '1', nickname: 'User', message: 'Test', date: new Date() }
+        ]);
+        actionMock = { type: 'GET_MESSAGES' };
+        const middleware = chatMiddleware(storeMock);
+
+        await middleware(nextMock)(actionMock);
+
+        expect(chatApi.getMessagesList).toHaveBeenCalled();
+        expect(storeMock.dispatch).toHaveBeenCalledWith({
+            type: 'RECEIVE_MESSAGES',
+            payload: [
+                { id: '1', nickname: 'User', message: 'Test', date: new Date() }
+            ]
+        });
+    });
+
 });
---
+++ src/__tests__/middlewareCaseDelete.test.ts
@@ -0,0 +1,40 @@
+import chatMiddleware from '../middleware';
+import { Store } from '../ChatStore';
+import { jest } from '@jest/globals';
+import * as chatApi from '../chat';
+import {
+    ChatActionTypes,
+    SendMessageAction,
+} from '../ChatReducer';
+
+jest.mock('../chat');
+
+describe('chatMiddleware', () => {
+    let storeMock: Store;
+    let nextMock: jest.Mock;
+    let actionMock: ChatActionTypes;
+
+    beforeEach(() => {
+        storeMock = { dispatch: jest.fn() } as unknown as Store;
+        nextMock = jest.fn();
+        jest.mocked(chatApi.deleteMessageId).mockResolvedValue();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should handle DELETE_MESSAGE action successfully', async () => {
+        const messageId = '1';
+        actionMock = { type: 'DELETE_MESSAGE', payload: messageId };
+        const middleware = chatMiddleware(storeMock);
+    
+        await middleware(nextMock)(actionMock);
+    
+        expect(chatApi.deleteMessageId).toHaveBeenCalledWith(messageId);
+        expect(storeMock.dispatch).toHaveBeenCalledWith({
+          type: 'MESSAGE_DELETED',
+          payload: messageId,
+        });
+      });
+});
